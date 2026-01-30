e7e35dd0aabd23212b988daf5c6cdf7801b55ed6
lzy <1339181097@qq.com>
2026-01-30 16:30:59 +0800

    新添注释

--- dataset/lm_dataset.py
+++ dataset/lm_dataset.py
@@ -18,11 +18,15 @@ class PretrainDataset(Dataset):
 
     def __getitem__(self, index):
         sample = self.samples[index] # type: ignore
+        # 手动添加特殊符号bos，eos
         tokens = self.tokenizer(str(sample['text']), add_special_tokens=False, max_length=self.max_length - 2, truncation=True).input_ids
         tokens = [self.tokenizer.bos_token_id] + tokens + [self.tokenizer.eos_token_id]
+        # pad为还没到达最大长度的部分
         input_ids = tokens + [self.tokenizer.pad_token_id] * (self.max_length - len(tokens))
         input_ids = torch.tensor(input_ids, dtype=torch.long)
+        # shift在model里进行
         labels = input_ids.clone()
+        # pad填上-100，计算loss时自动忽略
         labels[input_ids == self.tokenizer.pad_token_id] = -100
         return input_ids, labels
     
